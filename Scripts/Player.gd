02bea576d3db7ca3b72ac49c90ce49d7af68fbbd
Chase McNamara <ChasingAir@users.noreply.github.com>
2019-03-31 15:43:13 -0700

    added test build

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,3 +9,8 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseMotion: #If there is mouse input
 		$Head.rotate_y(deg2rad(-event.relative.x * .05 * mouse_sensitivity)) #Rotate head
+		
+		var change = event.relative.y * .05 * mouse_sensitivity
+		if change + camera_angle < 90 and change + camera_angle > -90:
+			$Head/Camera.rotate_z(deg2rad(change))
+			camera_angle += change
