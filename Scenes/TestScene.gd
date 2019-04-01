a0fb609f785b954786e69fe7131d520e94affc79
Chase McNamara <ChasingAir@users.noreply.github.com>
2019-03-31 19:27:01 -0700

    imported models

--- Scenes/TestScene.gd
+++ Scenes/TestScene.gd
@@ -1,5 +1,7 @@
 extends Spatial
 
+var captured = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) #Captures the cursor, makes it invisible
@@ -11,3 +13,10 @@ func _process(delta):
 		get_tree().quit() #Exit the game
 	if Input.is_action_just_pressed("dev_restart"):
 		get_tree().reload_current_scene() #Restarts the current scene
+	if Input.is_action_just_pressed("ui_accept"):
+		if captured == false:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) #Captures the cursor, makes it invisible
+			captured = true
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) #Make the cusor visable, releases it
+			captured = false
