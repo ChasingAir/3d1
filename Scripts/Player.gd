8ba6043acfaecc999762bb3d51615a6eda5471b5
Chase McNamara <ChasingAir@users.noreply.github.com>
2019-04-03 15:31:28 -0700

    lots of additions
    added new trees, modified jump, added crouch, fixed slope sliding issues, fixed jumping, updated testbuild1

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,8 @@ extends KinematicBody
 
 var camera_angle = 0
 var mouse_sensitivity = 5
+var camera_change = Vector2()
+
 var velocity = Vector3()
 var direction = Vector3()
 
@@ -11,28 +13,27 @@ const FLY_ACCEL = 4
 
 #walk variables
 var gravity = -9.8 * 3
-const MAX_SPEED = 15
-const MAX_RUNNING_SPEED = 30
+const MAX_SPEED = 10
+const MAX_RUNNING_SPEED = 18
+const MAX_CROUCH_SPEED = 3
 const ACCEL = 2
-const DEACCEL = 6
+const DEACCEL = 10
 
 #jumping
-var jump_height = 15
+var jump_height = 10
+var in_air = 0
+var has_contact = false
 
 func _ready():
 	pass
 
 func _physics_process(delta):
+	aim()
 	walk(delta)
 
 func _input(event):
 	if event is InputEventMouseMotion: #If there is mouse input
-		$Head.rotate_y(deg2rad(-event.relative.x * .05 * mouse_sensitivity)) #Rotate head
-		
-		var change = event.relative.y * .05 * mouse_sensitivity
-		if change + camera_angle < 90 and change + camera_angle > -90:
-			$Head/Camera.rotate_z(deg2rad(change))
-			camera_angle += change
+		camera_change = event.relative
 
 func walk(delta):
 	#reset the direction of the player
@@ -52,21 +53,34 @@ func walk(delta):
 	#normalize the direction
 	direction = direction.normalized()
 	
+	if (is_on_floor()):
+		has_contact = true
+	else:
+		if !$TailCast.is_colliding():
+			has_contact = false
+	if (has_contact and !is_on_floor()):
+		move_and_collide(Vector3(0,-1,0))
+	
+	#apply gravity
 	velocity.y += gravity * delta
 	
 	var temp_velocity = velocity
 	temp_velocity.y = 0
 	
+	#figure out what speed to be moving
 	var speed
-	if Input.is_action_pressed("move_sprint"):
+	if Input.is_action_pressed("move_sprint"): #if sprint key, sprint speed. this takes precedence
 		speed = MAX_RUNNING_SPEED
 	else:
-		speed = MAX_SPEED
+		if Input.is_action_pressed("crouch"): #if crouch key, and not sprint key, crouch speed
+			speed = MAX_CROUCH_SPEED
+		else:
+			speed = MAX_SPEED #if neither modifier key is pressed, use base speed
 	
 	#where would the player go at max speed
 	var target = direction * speed
 	
-	var acceleration
+	var acceleration #this variable will hold current acceleration, using 
 	if direction.dot(temp_velocity) > 0:
 		acceleration = ACCEL
 	else:
@@ -78,12 +92,13 @@ func walk(delta):
 	velocity.x = temp_velocity.x
 	velocity.z = temp_velocity.z
 	
-	#move
-	velocity = move_and_slide(velocity, Vector3(0,1,0))
-	
 	#jumping
-	if Input.is_action_just_pressed("jump"):
+	if has_contact and Input.is_action_just_pressed("jump"):
 		velocity.y = jump_height
+		has_contact = false
+	
+	#move
+	velocity = move_and_slide(velocity, Vector3(0,1,0), 0.15)
 
 func fly(delta):
 	#reset the direction of the player
@@ -111,3 +126,13 @@ func fly(delta):
 	
 	#move
 	move_and_slide(velocity)
+
+func aim():
+	if camera_change.length() > 0:
+		$Head.rotate_y(deg2rad(-camera_change.x * .05 * mouse_sensitivity)) #Rotate head
+		
+		var change = camera_change.y * .05 * mouse_sensitivity
+		if change + camera_angle < 90 and change + camera_angle > -90:
+			$Head/Camera.rotate_z(deg2rad(change))
+			camera_angle += change
+		camera_change = Vector2()
